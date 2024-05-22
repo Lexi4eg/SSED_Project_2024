c3c29abcdc0387963ced38cca395d2f908e19859
Lexi4eg <felix.prattes@gmail.com>
2024-05-22 21:01:07 +0200

    Table added

---
+++ SWP_Project_2024/Pages/Book.cs
@@ -0,0 +1,13 @@
+namespace SWP_Project_2024.Pages;
+
+public class Book
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    public string Isbn { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}

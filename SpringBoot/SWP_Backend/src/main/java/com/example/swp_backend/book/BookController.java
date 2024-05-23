62148dbe95889adbff168c17d563a7b1d52ed1a7
Lexi4eg <felix.prattes@gmail.com>
2024-05-23 08:26:40 +0200

    Working CORS

--- SpringBoot/SWP_Backend/src/main/java/com/example/swp_backend/book/BookController.java
+++ SpringBoot/SWP_Backend/src/main/java/com/example/swp_backend/book/BookController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5138")
 @RestController
 @RequestMapping("/api/v1/book")
 public class BookController {

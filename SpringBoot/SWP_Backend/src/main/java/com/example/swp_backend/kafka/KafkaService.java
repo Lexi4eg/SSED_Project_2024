4be9db0a73be7d288fa64d4eafaa06a1c9deb019
Lexi4eg <felix.prattes@gmail.com>
2024-05-23 20:45:21 +0200

    Updated Docker with Kafka

---
+++ SpringBoot/SWP_Backend/src/main/java/com/example/swp_backend/kafka/KafkaService.java
@@ -0,0 +1,23 @@
+package com.example.swp_backend.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}

cd38ffbb6de9afacb20e91f9bb322f007a36d55a
Felix Prattes <felix.prattes@gmail.com>
2024-05-27 20:35:33 +0200

    Question Leaderboard API Changed

---
+++ SWP_Project_2024/Pages/QuestionLeaderboard.cs
@@ -0,0 +1,21 @@
+﻿namespace SWP_Project_2024.Pages;
+
+public class QuestionLeaderboard
+{
+    public long Id { get; set; }
+    public String username { get; set; }
+    public int score { get; set; }
+    
+    public QuestionLeaderboard(long id, String username, int score)
+    {
+        this.Id = id;
+        this.username = username;
+        this.score = score;
+    }
+    
+    public QuestionLeaderboard()
+    {
+        
+    }
+    
+}

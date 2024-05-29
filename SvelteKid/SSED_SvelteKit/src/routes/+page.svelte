3dcd2004172bc2b8133da7202813da0d655c9c3b
Felix Prattes <felix.prattes@gmail.com>
2024-05-29 11:58:27 +0200

    New Svelte Frontend for Testing

---
+++ SvelteKid/SSED_SvelteKit/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="SSED Project" />
+</svelte:head>
+
+<section>
+	<div class="bg-yellow-500"></div>
+	hi
+
+
+</section>
+

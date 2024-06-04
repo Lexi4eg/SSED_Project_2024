d15ac34910e1f7aebb6e81fe14f1b5b0b665107f
Felix Prattes <felix.prattes@gmail.com>
2024-06-04 21:01:41 +0200

    Leaderboard

---
+++ Nuxt_SSED_Project/pages/leaderboard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+interface Leaderboard {
+  id: number
+  username : string
+  score: number
+}
+const { data } = await useFetch<Leaderboard[]>('http://localhost:8080/api/v1/leaderboard')
+
+</script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-3xl ">Leaderboard</h1>
+    <UTable  :rows="data || []" />
+  </div>
+</template>
---
+++ Nuxt_SSED_Project/pages/questions.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+
+
+const { data } = await useFetch('/api/count')
+
+</script>
+
+<template>
+  <h1>
+    Hello World
+  </h1>
+</template>
